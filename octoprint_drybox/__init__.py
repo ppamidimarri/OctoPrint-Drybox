16845dce8c4688ac544d36c249af21211826d492
ppamidimarri <pavan.pamidimarri@gmail.com>
2019-02-02 22:45:09 -0800

    Reverting to simpler design, no navbar, just one URL. Drybox will manage its own navigation so more complex flows are possible.

--- octoprint_drybox/__init__.py
+++ octoprint_drybox/__init__.py
@@ -9,21 +9,13 @@ class DryboxPlugin(octoprint.plugin.StartupPlugin,
 			octoprint.plugin.SettingsPlugin,
 			octoprint.plugin.AssetPlugin):
 	def on_after_startup(self):
-		self._logger.info("Drybox says hello! URL: %s" % self._settings.get(["drybox_profiles"]))
+		self._logger.info("Drybox loaded, with URL: %s" % self._settings.get(["url"]))
 
 	def get_settings_defaults(self):
-		return dict(
-			drybox_profiles=[{
-				'name': 'Default',
-				'url': 'http://192.168.86.181/drybox/humidity.html',
-				'isButtonEnabled': 'true'
-			}]
-		)
+		return dict(url='http://192.168.86.181/index.html')
 
 	def get_template_configs(self):
-		return [
-			dict(type="settings", custom_bindings=False)
-		]
+		return [dict(type="settings", custom_bindings=False)]
 
 	def get_assets(self):
 		return dict(
